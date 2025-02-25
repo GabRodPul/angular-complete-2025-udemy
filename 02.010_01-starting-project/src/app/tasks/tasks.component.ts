6b05088e2d56ea3c366b87ad368671a7fb304f33
Gabriel Rodríguez <103140108+GabRodPul@users.noreply.github.com>
2025-02-25 13:42:39 +0000

    02.045

--- 02.010_01-starting-project/src/app/tasks/tasks.component.ts
+++ 02.010_01-starting-project/src/app/tasks/tasks.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { TaskComponent } from "../task/task.component";
 
 @Component({
@@ -9,5 +9,33 @@ import { TaskComponent } from "../task/task.component";
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  name = input<string | undefined>();
+  userId = input<string>();
+  name = input<string>();
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary:
+        'Learn all the basic and advanced features of Angular & how to apply them.',
+      dueDate: '2025-12-31',
+    },
+    {
+      id: 't2',
+      userId: 'u3',
+      title: 'Build first prototype',
+      summary: 'Build a first prototype of the online shop website',
+      dueDate: '2024-05-31',
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Prepare issue template',
+      summary:
+        'Prepare and describe an issue template which will help with project management',
+      dueDate: '2024-06-15',
+    },
+  ]
+
+  selectedUserTasks = computed(() => this.tasks.filter((t) => t.userId === this.userId()));
 }
