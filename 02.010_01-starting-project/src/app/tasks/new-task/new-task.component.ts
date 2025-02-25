a89ff9c9b04eb656d7d3c2e2045771959c6e846c
Gabriel Rodríguez <103140108+GabRodPul@users.noreply.github.com>
2025-02-25 14:47:12 +0000

    02.052

--- 02.010_01-starting-project/src/app/tasks/new-task/new-task.component.ts
+++ 02.010_01-starting-project/src/app/tasks/new-task/new-task.component.ts
@@ -1,14 +1,18 @@
 import { Component, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
-  cancel = output<void>();
+  cancel  = output<void>();
+  title   = "";
+  summary = "";
+  dueDate = "";
 
   onCancel() {
     this.cancel.emit();
