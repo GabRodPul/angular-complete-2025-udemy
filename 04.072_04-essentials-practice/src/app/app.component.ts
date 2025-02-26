3d0dd1d650bb48a5dc4dfa6275c91482c24bfdff
Gabriel Rodríguez <103140108+GabRodPul@users.noreply.github.com>
2025-02-26 12:23:58 +0000

    04.080-082

--- 04.072_04-essentials-practice/src/app/app.component.ts
+++ 04.072_04-essentials-practice/src/app/app.component.ts
@@ -1,20 +1,19 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserInputComponent } from "./user-input/user-input.component";
+import { InvestmentInput, InvestmentResult } from './investment.model';
+import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  calculateInvestmentResults(data:{
-    initialInvestment: number,
-    annualInvestment: number,
-    expectedReturn: number,
-    duration: number,
-  }) {
+  results?: InvestmentResult[];
+
+  onCalculate(data: InvestmentInput) {
     const { initialInvestment, annualInvestment, expectedReturn, duration } = data;
     const annualData = [];
     let investmentValue = initialInvestment;
@@ -35,6 +34,6 @@ export class AppComponent {
       });
     }
   
-    return annualData;
+    this.results = annualData;
   }  
 }
--- 04.072_04-essentials-practice/src/app/user-input/user-input.component.ts
+++ 04.072_04-essentials-practice/src/app/user-input/user-input.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { InvestmentInput } from "../investment.model";
 
 @Component({
   selector: 'app-user-input',
@@ -9,12 +10,18 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  calculate = output<InvestmentInput>();
   fInitialInvestment = "0";
   fAnnualInvestment  = "0";
   fExpectedReturn    = "5";
   fDuration          = "10";
 
   onSubmit() {
-
+    this.calculate.emit({
+      initialInvestment:  +this.fInitialInvestment,
+      annualInvestment:   +this.fAnnualInvestment,
+      expectedReturn:     +this.fExpectedReturn,
+      duration:           +this.fDuration,
+    })
   }
 }
