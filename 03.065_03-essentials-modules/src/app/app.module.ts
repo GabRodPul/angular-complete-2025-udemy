d8ce975355c0b8cbc54bc6a8f2da1f71963c67ea
Gabriel Rodríguez <103140108+GabRodPul@users.noreply.github.com>
2025-02-26 10:57:31 +0000

    03.071

--- 03.065_03-essentials-modules/src/app/app.module.ts
+++ 03.065_03-essentials-modules/src/app/app.module.ts
@@ -5,10 +5,10 @@ import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
 import { TaskComponent } from "./tasks/task/task.component";
-import { CardComponent } from "./shared/card/card.component";
 import { TasksComponent } from "./tasks/tasks.component";
 import { NewTaskComponent } from "./tasks/new-task/new-task.component";
 import { FormsModule } from "@angular/forms";
+import { SharedModule } from "./shared/shared.module";
 
 @NgModule({
   declarations: [
@@ -16,11 +16,10 @@ import { FormsModule } from "@angular/forms";
     HeaderComponent, 
     UserComponent, 
     TaskComponent,
-    CardComponent,
     TasksComponent,
     NewTaskComponent
   ],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, FormsModule]
+  imports: [BrowserModule, FormsModule, SharedModule]
 })
 export class AppModule {}
