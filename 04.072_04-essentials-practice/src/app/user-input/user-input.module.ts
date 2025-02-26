4150dc8fe3fe317534d3721a3b79ecdf59c06dbf
Gabriel Rodríguez <103140108+GabRodPul@users.noreply.github.com>
2025-02-26 13:45:31 +0000

    04.088

---
+++ 04.072_04-essentials-practice/src/app/user-input/user-input.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserInputComponent } from './user-input.component';
+import { FormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [UserInputComponent],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  exports: [UserInputComponent]
+})
+export class UserInputModule { }
