b946349776dc9acbb017b1cd97fd485eb80ebdc5
Gabriel Rodríguez <103140108+GabRodPul@users.noreply.github.com>
2025-02-27 14:31:17 +0000

    06.146

--- 06.145_19-custom-2w-binding-starting-project/src/app/rect/rect.component.ts
+++ 06.145_19-custom-2w-binding-starting-project/src/app/rect/rect.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, input, Output, output } from '@angular/core';
+import { Component, EventEmitter, input, model, Output, output } from '@angular/core';
 
 export const DEFAULT_RECT_SIZE = {
   width: '100',
@@ -14,11 +14,12 @@ export const DEFAULT_RECT_SIZE = {
 })
 export class RectComponent {
   // Todo: Implement custom two-way binding
-  size        = input.required<{ width: string, height: string }>();
+  // size        = input.required<{ width: string, height: string }>();
   // sizeChange  = output<{ width: string, height: string }>();
-  @Output() sizeChange = new EventEmitter<{ width: string, height: string }>();
+  // @Output() sizeChange = new EventEmitter<{ width: string, height: string }>();
+  size = model.required<{ width: string, height: string }>();
 
   onReset() {
-    this.sizeChange.emit({ width: "100", height: "100" }); // DEFAULT_RECT_SIZE not working...
+    this.size.set({ width: "100", height: "100" }); // DEFAULT_RECT_SIZE not working...
   }
 }
