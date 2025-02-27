f96fdbd6e25fa675bc5b84736628e023549d0b47
Gabriel Rodríguez <103140108+GabRodPul@users.noreply.github.com>
2025-02-27 08:37:07 +0000

    06.111

--- 06.095_06-cmp-deep-dive/src/app/shared/button/button.component.ts
+++ 06.095_06-cmp-deep-dive/src/app/shared/button/button.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'button[appButton]',
+  selector: 'button[appButton], aappButton]',
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
