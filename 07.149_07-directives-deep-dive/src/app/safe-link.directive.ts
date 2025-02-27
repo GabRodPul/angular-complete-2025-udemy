532a685b340ba966e6b9c81774f12459acb1d84a
Gabriel Rodríguez <103140108+GabRodPul@users.noreply.github.com>
2025-02-27 15:52:26 +0000

    07.153-155

--- 07.149_07-directives-deep-dive/src/app/safe-link.directive.ts
+++ 07.149_07-directives-deep-dive/src/app/safe-link.directive.ts
@@ -1,13 +1,24 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, inject, input } from '@angular/core';
 
 @Directive({
   selector: 'a[appSafeLink]',
   standalone: true
 })
 export class SafeLinkDirective {
+  paramFrom = input("myapp", { alias: "appSafeLink" });
+  private hostRef = inject<ElementRef<HTMLAnchorElement>>(ElementRef);
 
   constructor() {
     console.log("SafeLinkDirective is active!");
   }
 
+  @HostListener("click", ["$event"]) onConfirm(event: MouseEvent) {
+    if (window.confirm("Do you want to leave the app?")) {
+      const t = this.hostRef.nativeElement;
+      t.href  =`${t.href}/?from=${this.paramFrom()}`;
+      return;
+    }
+
+    event.preventDefault();
+  }
 }
