1792e02f0e4b0b8f02b590f53ae92bd4cb13784c
Gabriel Rodríguez <103140108+GabRodPul@users.noreply.github.com>
2025-02-27 08:54:25 +0000

    06.115

--- 06.095_06-cmp-deep-dive/src/app/shared/control/control.component.ts
+++ 06.095_06-cmp-deep-dive/src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import { Component, HostBinding, HostListener, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -8,10 +8,16 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
   host: {
-    class: "control"
+    class: "control",
+    "(click)": "onclick()",
   }
 })
 export class ControlComponent {
+  // @HostBinding("class") className = "control";
   label = input.required<string>();
-  
+
+  // @HostListener("click") 
+  onClick() {
+    console.log("Clicked")
+  }
 }
