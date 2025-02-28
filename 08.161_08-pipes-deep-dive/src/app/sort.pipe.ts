24e6b7aaf681046fcac441239e8779fe174617f9
Gabriel Rodríguez <103140108+GabRodPul@users.noreply.github.com>
2025-02-28 09:28:31 +0000

    08.168

---
+++ 08.161_08-pipes-deep-dive/src/app/sort.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'sort',
+  standalone: true
+})
+export class SortPipe implements PipeTransform {
+
+  transform(value: any[], order: "asc" | "desc" = "asc"): typeof value {
+    const result = [...value];
+    
+    type Val = typeof value[number];
+    const cmp: (a: Val, b: Val) => 1 | -1
+      = order == "asc"
+      ? (a, b) => a > b ? 1 : -1
+      : (a, b) => a < b ? 1 : -1;
+
+    result.sort(cmp);
+    return result;
+  }
+
+}
