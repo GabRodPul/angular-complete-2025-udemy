19bcb97fcd74e608e9c6359173c698aa6404705c
Gabriel Rodríguez <103140108+GabRodPul@users.noreply.github.com>
2025-02-28 13:42:38 +0000

    11.208

--- 11.206_11-rxjs/src/app/app.component.ts
+++ 11.206_11-rxjs/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
 import { interval, map } from 'rxjs';
 
 @Component({
@@ -8,16 +8,27 @@ import { interval, map } from 'rxjs';
 })
 export class AppComponent implements OnInit {
   private destroy = inject(DestroyRef);
+  count = signal(0);
+
+  constructor() {
+    effect(() => {
+      console.log("CLICKS: " + this.count())
+    });
+  }
 
   ngOnInit(): void {
-   const sub = interval(1000).pipe(
-    map((n) => n * 2)
-   ).subscribe({
-    next: (n) => console.log(n)
-   });
+  //  const sub = interval(1000).pipe(
+  //   map((n) => n * 2)
+  //  ).subscribe({
+  //   next: (n) => console.log(n)
+  //  });
 
-   this.destroy.onDestroy(() => {
-    sub.unsubscribe();
-   })
+  //  this.destroy.onDestroy(() => {
+  //   sub.unsubscribe();
+  //  })
+ }
+
+ onClick() {
+  this.count.update(c => c+1);
  }
 }
