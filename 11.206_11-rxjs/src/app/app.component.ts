da9e5aae5f4cb39c272f30a18f1972c04dacb78c
Gabriel Rodríguez <103140108+GabRodPul@users.noreply.github.com>
2025-02-28 13:52:16 +0000

    11.210

--- 11.206_11-rxjs/src/app/app.component.ts
+++ 11.206_11-rxjs/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
 import { interval, map } from 'rxjs';
 
 @Component({
@@ -9,26 +10,34 @@ import { interval, map } from 'rxjs';
 export class AppComponent implements OnInit {
   private destroy = inject(DestroyRef);
   count = signal(0);
+  count$ = toObservable(this.count);
 
   constructor() {
-    effect(() => {
-      console.log("CLICKS: " + this.count())
-    });
+    // effect(() => {
+    //   console.log("CLICKS: " + this.count())
+    // });
   }
 
   ngOnInit(): void {
-   const sub = interval(1000).pipe(
-    map((n) => n * 2)
-   ).subscribe({
-    next: (n) => console.log(n)
-   });
+    //  const sub = interval(1000).pipe(
+    //   map((n) => n * 2)
+    //  ).subscribe({
+    //   next: (n) => console.log(n)
+    //  });
 
-   this.destroy.onDestroy(() => {
-    sub.unsubscribe();
-   })
- }
+    //  this.destroy.onDestroy(() => {
+    //   sub.unsubscribe();
+    //  })
+    const sub = this.count$.subscribe({
+      next: (n) => console.log("CLICKS: " + this.count())
+    });
 
- onClick() {
-  this.count.update(c => c+1);
- }
+    this.destroy.onDestroy(() => {
+      sub.unsubscribe();
+    })
+  }
+
+  onClick() {
+    this.count.update(c => c + 1);
+  }
 }
