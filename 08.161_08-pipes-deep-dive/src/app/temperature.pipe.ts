4b61e6f0a4a47c4dd9650f1b239e3721924ae387
Gabriel Rodríguez <103140108+GabRodPul@users.noreply.github.com>
2025-02-28 09:08:49 +0000

    08.166

--- 08.161_08-pipes-deep-dive/src/app/temperature.pipe.ts
+++ 08.161_08-pipes-deep-dive/src/app/temperature.pipe.ts
@@ -1,17 +1,30 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+type Temps = "c" | "f"
+
 @Pipe({
   name: 'temp',
   standalone: true
 })
 export class TemperaturePipe implements PipeTransform {
-  transform(value: string | number, ...args: unknown[]): unknown {
-    let val = typeof value === "string"
+  transform(value: string | number, inp: Temps, out?: Temps): unknown {
+    const val = typeof value === "string"
             ? parseFloat(value)
             : value;
 
-    val = val * (9 / 5) + 32;
+    let temp;
+    switch (true) {
+      case inp === "c" && out === "f": {
+        temp = val * (9 / 5) + 32;
+      } break;
 
-    return `${val} ºF`;
+      case inp === "f" && out === "c": {
+        temp = (val - 32) * (5 / 9);
+      } break;
+    }
+
+    const letter = out ? out : inp;
+
+    return `${temp} º${letter.toUpperCase()}`;
   }
 }
