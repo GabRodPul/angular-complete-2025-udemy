c49b56d2dd6741a7084b0687bd7ea8308a1a7dea
Gabriel Rodríguez <103140108+GabRodPul@users.noreply.github.com>
2025-02-28 14:10:37 +0000

    11.212

--- 11.206_11-rxjs/src/app/app.component.ts
+++ 11.206_11-rxjs/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
-import { interval, map } from 'rxjs';
+import { interval, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +13,20 @@ export class AppComponent implements OnInit {
   count$ = toObservable(this.count);
   interval$ = interval(1000);
   intervalSig = toSignal(this.interval$, { initialValue: 0, manualCleanup: false });
+  customIntv$ = new Observable((sub) => {
+    let execCount = 0;
+    const intv = setInterval(() => {
+      // sub.error();
+      if (execCount > 3) {
+        clearInterval(intv);
+        sub.complete();
+        return;
+      }
+      console.log("Emitting new value...");
+      sub.next({ message: "New value" });
+      execCount++;
+    }, 2000);
+  });
 
   constructor() {
     // effect(() => {
@@ -30,6 +44,11 @@ export class AppComponent implements OnInit {
     //  this.destroy.onDestroy(() => {
     //   sub.unsubscribe();
     //  })
+    this.customIntv$.subscribe({
+      next:     (n)   => console.log(n),
+      complete: ()    => console.log("COMPLETED"),
+      error:    (err) => console.log(err),
+    })
     const sub = this.count$.subscribe({
       next: (n) => console.log("CLICKS: " + this.count())
     });
