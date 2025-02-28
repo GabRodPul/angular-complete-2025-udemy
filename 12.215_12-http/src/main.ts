c7756bcecde7b22fb4157b88cf7e41344b6c44ef
Gabriel Rodríguez <103140108+GabRodPul@users.noreply.github.com>
2025-02-28 16:32:10 +0000

    12.234-236

--- 12.215_12-http/src/main.ts
+++ 12.215_12-http/src/main.ts
@@ -1,9 +1,31 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpEventType, HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { tap } from 'rxjs';
 
+const logIntrcpt = (req: HttpRequest<unknown>, next: HttpHandlerFn) => {
+  // const _req = req.clone({
+  //   headers: req.headers.set("X-DEBUG", "TESTING")
+  // });
+  console.log("OUTGOING REQUEST");
+  console.log(req);
+  return next(req)
+    .pipe(
+      tap({
+        next: event => {
+          if (event.type === HttpEventType.Response) {
+            console.log("INCOMING RES");
+            console.log(event.status);
+            console.log(event.body);
+          }
+        }
+      })
+    );
+}
 
 bootstrapApplication(AppComponent, {
-  providers: [ provideHttpClient() ]
+  providers: [ provideHttpClient(
+    withInterceptors([logIntrcpt])
+  )]
 }).catch((err) => console.error(err));
