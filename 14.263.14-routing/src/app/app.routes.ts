8dd90a577756508029604ebf93b06bdf285d8810
Gabriel Rodríguez <103140108+GabRodPul@users.noreply.github.com>
2025-03-06 14:42:55 +0000

    14.281

--- 14.263.14-routing/src/app/app.routes.ts
+++ 14.263.14-routing/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
 import { TasksComponent } from "./tasks/tasks.component";
 import { NewTaskComponent } from "./tasks/new-task/new-task.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
+import { userRoutes } from "./users/user/user.routes";
 
 export const routes: Routes = [
   { 
@@ -15,15 +16,7 @@ export const routes: Routes = [
   { 
     path: "users/:userId", 
     component: UserTasksComponent,
-    children: [
-      { 
-        path: "", 
-        redirectTo: "tasks",
-        pathMatch: "prefix"
-      },
-      { path: "tasks",     component: TasksComponent },
-      { path: "tasks/new", component: NewTaskComponent },
-    ]
+    children: userRoutes
   },
   {
     path: "**",
