6729b0292a5cb50aa8455bd612e86e92c6e31444
Gabriel Rodríguez <103140108+GabRodPul@users.noreply.github.com>
2025-03-06 12:25:26 +0000

    13.259

--- 13.237_13-forms/src/app/auth/signup/signup.component.ts
+++ 13.237_13-forms/src/app/auth/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 type Role = "student"
           | "teacher"
@@ -38,13 +38,18 @@ export class SignupComponent {
     }),
     firstName:  new FormControl("", { validators: [ Validators.required ] }),
     lastName:   new FormControl("", { validators: [ Validators.required ] }),
-    address: new FormGroup({
+    address:    new FormGroup({
       street:     new FormControl("", { validators: [ Validators.required ] }),
       number:     new FormControl("", { validators: [ Validators.required ] }),
       postalCode: new FormControl("", { validators: [ Validators.required ] }),
       city:       new FormControl("", { validators: [ Validators.required ] }),
     }),
     role:       new FormControl<Role>("student", { validators: [ Validators.required ] }),
+    source:     new FormArray([
+      new FormControl(false),
+      new FormControl(false),
+      new FormControl(false),
+    ]),
     agree:      new FormControl(false, { validators: [ 
       Validators.required,
       (ctrl) => ctrl.value // Agree = true
